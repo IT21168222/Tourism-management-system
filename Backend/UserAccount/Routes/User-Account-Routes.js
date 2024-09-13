45f7c979e981adea3f04e9ef4606b55852939e57
Saranga Gunasekara <99276930+IT21168222@users.noreply.github.com>
2024-09-14 01:09:36 +0530

    Update User-Account-Routes.js

--- Backend/UserAccount/Routes/User-Account-Routes.js
+++ Backend/UserAccount/Routes/User-Account-Routes.js
@@ -9,17 +9,11 @@ import {
   updateCount,
   updateCount2,
   updateUser,
+  limiter,
 } from "../Controllers/User-Account-Controller.js";
 import multer from "multer";
 import path from "path";
 const router = express.Router();
-const rateLimit = require("express-rate-limit");
-
-const registerLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 5,
-  message: "Too many accounts created from this IP, please try again later",
-});
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -30,29 +24,14 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({
-  limits: { fileSize: 1 * 1024 * 1024 }, // Limit file size to 1 MB
-  fileFilter: (req, file, cb) => {
-    // Only accept certain image types (e.g., png, jpeg)
-    if (!file.mimetype.startsWith("image/")) {
-      return cb(new Error("Invalid file type"), false);
-    }
-    cb(null, true);
-  },
-}).single("ProfilePicture");
+const upload = multer({ storage: storage });
 
-// const upload = multer({ storage: storage });
-
-// router.post(
-//   "/usersignup",
-//   registerLimiter,
-//   upload.single("ProfilePicture"),
-//   UserRegister
-// );
-
-const limiter = require("../Controllers/limiter");
-
-router.post("/usersignup", limiter, UserRegister);
+router.post(
+  "/usersignup",
+  limiter,
+  upload.single("ProfilePicture"),
+  UserRegister
+);
 router.post("/usersignin", Signin);
 router.delete("/usersignout", Signout);
 router.post("/Token", tokenRefresh);
